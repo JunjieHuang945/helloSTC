f6be2eb71b001439ec699986bb86172affbbd299
ubuntu <ubuntu@localhost.localdomain>
2021-07-13 11:30:12 +0800

    通过定义宏形式定义寄存器名称

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include<8052.h>
 #include "delay.h"
 
+//通过宏形式定义寄存器名称
+#define LED P1_1
 unsigned int j;
 //时间控制
 unsigned int ONOFFSET[24]={1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
@@ -14,13 +16,13 @@ void Ini1init(){
 }
 int main(){
 	unsigned int i;
-	P1_1=0;//初始化
+	LED = 0;//初始化
 	Ini1init();
 	//通断测试
 	for(i=0;i<3;i++){
-		P1_1=1;
+		LED = 1;
 		delay_s(1);
-		P1_1=0;
+		LED = 0;
 	}
 	while (1)
 	{
@@ -28,7 +30,7 @@ int main(){
 		{
 			j=0;
 		}
-		P1_1 = ONOFFSET[j];
+		LED = ONOFFSET[j];
 		delay_h(1);
 		j++;
 	}
@@ -36,6 +38,6 @@ int main(){
 }
 void Int1() __interrupt 2{
 	j = 0;
-	P1_1 = ONOFFSET[j];
+	LED = ONOFFSET[j];
 }
 
