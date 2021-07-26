2093bd62a09258082e3404e07cd003dfe0a121ba
ubuntu <ubuntu@localhost.localdomain>
2021-07-26 14:52:54 +0800

    基于时间周期定时控制LED状态

--- main.c
+++ main.c
@@ -3,30 +3,74 @@
 #include "timer.h"
 
 //通过宏形式定义寄存器名称
-#define LED P1_1
-//时间控制
-unsigned int ONOFFSET[24] = {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-unsigned int j = 0;
+#define LED P0_0
+//led关闭电平
+#define LED_STATUS_OFF 0
+//led打开电平
+#define LED_STATUS_ON 1
+//控制时间周期长度
+#define TIME_H_LENGTH 4
+//补光时间控制
+unsigned int ONOFFSET[TIME_H_LENGTH] = {0, 1, 2, 3, 4, 6};
+unsigned int timeD = 1;
 unsigned int timeH = 0;
 unsigned int timeM;
 unsigned int timeS;
 unsigned int i = 0;
+//重新设置LED状态函数
+void resetLEDStatus();
+//天数改变函数
+void timeDChange();
+//小时改变函数
+void timeHChange();
+//分钟改变函数
+void timeMChange();
+//秒钟改变函数
+void timeSChange();
+
 int main()
 {
-	LED = ONOFFSET[timeH]; //初始化
-	EA = 1;				   //闭合总中断开关
+	LED = LED_STATUS_OFF;
+	EA = 1; //闭合总中断开关
 	TIM0_Init(50000, 20);
+	//LED默认关闭
+	resetLEDStatus();
 	return 0;
+}
+
+void resetLEDStatus()
+{
+	unsigned int index = 0;
+	for (index = 0; index < TIME_H_LENGTH; index++)
+	{
+		int status = ONOFFSET[index];
+		if (status == timeH)
+		{
+			LED = LED_STATUS_ON;
+			return;
+		}
+	}
+	LED = LED_STATUS_OFF;
+}
+
+void timeDChange()
+{
+	timeD++;
+	if (timeD == 32)
+	{
+		timeD = 1;
+	}
 }
 
 void timeHChange()
 {
 	timeH++;
-	if (timeH == 60)
+	if (timeH == 24)
 	{
 		timeH = 0;
+		timeDChange();
 	}
-	LED = ONOFFSET[timeH];
+	resetLEDStatus();
 }
 
 void timeMChange()
@@ -35,6 +79,7 @@ void timeMChange()
 	if (timeM == 60)
 	{
 		timeM = 0;
+		timeHChange();
 	}
 }
 
@@ -50,7 +95,6 @@ void timeSChange()
 
 void TIM0_IRQHandler() __interrupt 1
 {
-	static int i;
 	TH0 = T0RH; //重新加载重载值
 	TL0 = T0RL;
 	i++;
